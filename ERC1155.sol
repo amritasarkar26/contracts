7b7c9d4b0db614a3ad05871d49c9f90270331d34
amritasarkar26 <165267996+amritasarkar26@users.noreply.github.com>
2024-03-31 12:30:36 +0530

    Update ERC1155.sol

--- ERC1155.sol
+++ ERC1155.sol
@@ -1,1 +1,19 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC1155Contract is ERC1155, Ownable {
+
+    constructor(string memory uri) ERC1155(uri) {
+        // Mint an initial supply of tokens to the owner
+        _mint(msg.sender, 1, 100000000000000000); // Minting 100 tokens with ID 1
+    }
+
+    function mint(address to, uint256 tokenId, uint256 amount) external onlyOwner {
+        _mint(to, tokenId, amount);
+    }
+}
+
